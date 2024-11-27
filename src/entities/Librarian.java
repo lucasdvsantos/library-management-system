1145c969c7651e6e7905e46a193dcbdee5a64636
Lucas David <santoslucasdavid@gmail.com>
2024-11-26 22:09:05 -0300

    Librarian entity added

---
+++ src/entities/Librarian.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Librarian {
+
+	private String name;
+	private int employeeId;
+	
+	public Librarian() {
+	}
+
+	public Librarian(String name, int employeeId) {
+		this.name = name;
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	
+	
+}
